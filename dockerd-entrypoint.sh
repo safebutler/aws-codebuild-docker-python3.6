29b3de1b14c18a36aa9b5998ba44f947d44bd4a4
nan-zhong <danny@safebutler.com>
2017-11-10 17:31:06 -0800

    Create dockerd-entrypoint.sh

---
+++ dockerd-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Copied from https://github.com/aws/aws-codebuild-docker-images/blob/master/ubuntu/docker/1.12.1/dockerd-entrypoint.sh
+set -e
+
+/usr/local/bin/dind docker daemon \
+	--host=unix:///var/run/docker.sock \
+	--host=tcp://0.0.0.0:2375 \
+	--storage-driver=overlay &>/var/log/docker.log &
+
+
+tries=0
+d_timeout=60
+until docker info >/dev/null 2>&1
+do
+	if [ "$tries" -gt "$d_timeout" ]; then
+                cat /var/log/docker.log
+		echo 'Timed out trying to connect to internal docker host.' >&2
+		exit 1
+	fi
+        tries=$(( $tries + 1 ))
+	sleep 1
+done
+
+eval "$@"
